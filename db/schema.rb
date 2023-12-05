2b10a90141c5493239b8a65ac1cc241306af3be7
mjd2266 Michael Danzi <mjd2266@columbia.edu>
2023-12-05 00:21:34 +0000

    added new db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_01_174332) do
+ActiveRecord::Schema.define(version: 2023_12_05_001741) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(version: 2023_11_01_174332) do
   end
 
   create_table "resumes", force: :cascade do |t|
-    t.string "title"
-    t.text "resume_text"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
