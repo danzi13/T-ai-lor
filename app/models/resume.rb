cce89f45b30b52f7db14ba37f3514d0741ed7c7a
mjd2266 Michael Danzi <mjd2266@columbia.edu>
2023-12-05 21:04:21 +0000

    added .env to gitignore

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,6 +1,7 @@
 class Resume < ActiveRecord::Base
   has_one_attached :attachment
 
+  attribute :title, :string unless column_names.include?('title')
   validates :attachment, presence: true, if: -> { resume_text.blank? }
   validates :resume_text, presence: true, if: -> { attachment.blank? }
 
--- db/migrate/20231205003549_add_resume_text_to_resumes.rb
+++
@@ -1,5 +0,0 @@
-class AddResumeTextToResumes < ActiveRecord::Migration[6.0]
-  def change
-    add_column :resumes, :resume_text, :text
-  end
-end
