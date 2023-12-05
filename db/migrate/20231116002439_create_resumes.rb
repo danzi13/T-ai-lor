c40d0ac90237b173a79215f306e718b8cd7e9397
mjd2266 Michael Danzi <mjd2266@columbia.edu>
2023-12-05 00:13:40 +0000

    trying to fix db

--- db/migrate/20231116002439_create_resumes.rb
+++
@@ -1,8 +0,0 @@
-class CreateResumes < ActiveRecord::Migration[6.0]
-  def change
-    create_table :resumes do |t|
-      t.string :title
-      t.text :resume_text
-    end
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_16_002439) do
+ActiveRecord::Schema.define(version: 2023_11_01_174332) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
